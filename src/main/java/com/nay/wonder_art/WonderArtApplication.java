7a252c458c5e2d0e1c5e9318ba3d59aabae190ba
Nlhmmh <nlh2012.8@gmail.com>
2023-07-08 21:14:49 +0900

    first commit

---
+++ src/main/java/com/nay/wonder_art/WonderArtApplication.java
@@ -0,0 +1,13 @@
+package com.nay.wonder_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nay/wonder_art/WonderArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nay.wonder_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
